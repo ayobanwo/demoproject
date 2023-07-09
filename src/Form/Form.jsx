77aa08e4f890fdf8165a8e0ff02c89b83f407c6f
ayobanwo <olubanwoayomide@gmail.com>
2023-07-09 23:25:06 +0100

    Radio buttons controlled input

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function Form() {
-    const [formData, setFormData] = React.useState({firstName: "", lastName: "", email: "", comments: "", isFriendly: true})
+    const [formData, setFormData] = React.useState({firstName: "", lastName: "", email: "", comments: "", isFriendly: true, employment: ""})
     
     console.log(formData)
     
@@ -45,8 +45,49 @@ export default function Form() {
             <input 
                 type="checkbox" 
                 id="isFriendly" 
+                onChange={handleChange}
+                checked={formData.isFriendly}
+                name="isFriendly"
             />
             <label htmlFor="isFriendly">Are you friendly?</label>
+            <br/><br/>
+            <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    checked={formData.employment === 'unemployed'}
+                    onChange={handleChange}
+                    value="unemployed"
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    checked={formData.employment === 'part-time'}
+                    onChange={handleChange}
+                    value="part-time"
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    checked={formData.employment === 'full-time'}
+                    onChange={handleChange}
+                    value="full-time"
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+            </fieldset>
         </form>
     )
 }
