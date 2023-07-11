bfdff9544e4297081fcc74a059b46f0af2bbf770
ayobanwo <olubanwoayomide@gmail.com>
2023-07-11 23:52:07 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Box from './Box/Box';
-import Counter from './Counter/Counter';
-import Form2 from './Form/Form2';
-import JohnState from './JohnState/JohnState';
-import JokeApp from './JokeApp/JokeApp';
+// import Box from './Box/Box';
+// import Counter from './Counter/Counter';
+// import Form2 from './Form/Form2';
+// import JohnState from './JohnState/JohnState';
+// import JokeApp from './JokeApp/JokeApp';
 import MemeGen from './MemeGen/MemeGen';
 
 function App() {
